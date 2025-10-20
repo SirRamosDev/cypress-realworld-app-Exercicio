25337f45021ff6e06ddfedb10b27373580345445
Mateus Souza <mateussouza@Mateuss-MacBook-Air.local>
2025-10-20 12:14:17 -0300

    Exercicio 1

---
+++ cypress/pages/LoginPages.js
@@ -0,0 +1,29 @@
+class LoginPage{
+    selectorList(){
+        const selectors = {
+            usernameField: '[name="username"]',
+            passwordField: '[name="password"]',
+            loginButton: '[data-test="signin-submit"]',
+            errorMessageAlert: '[data-test="signin-error"]'
+        }
+        return selectors
+
+    }
+    acessLoginPage(){
+        cy.visit('/signin')
+    }
+
+    loginWithUser(username,password){
+        cy.get(this.selectorList().usernameField).type(username)
+        cy.get(this.selectorList().passwordField).type(password)
+        cy.get(this.selectorList().loginButton).click()
+    }
+    checkInvalidAccess(){
+        cy.get(this.selectorList().errorMessageAlert)
+        .should('be.visible')
+        .and('contain','Username or password is invalid')
+
+    }
+}
+
+export default LoginPage()
