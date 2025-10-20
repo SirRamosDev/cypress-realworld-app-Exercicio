67159be475d6647b7e45d134fdd6fb512adf1e05
Mateus Souza <mateussouza@Mateuss-MacBook-Air.local>
2025-10-20 12:59:55 -0300

    Exercicio 01

---
+++ cypress/e2e/registerTest.spec.js
@@ -0,0 +1,22 @@
+import userData from '../fixtures/userData.json';
+import RegisterPage from '../pages/registerPage';
+
+const registerPage = new RegisterPage()
+
+describe('Real Word App - Testes de Registro de Usuário', () => {
+
+    it('Registro de novo usuário com sucesso', () => {
+        const u = userData.newUser
+        registerPage.accessRegisterPage()
+        registerPage.fillRegistrationForm(u.firstName, u.lastName, u.username, u.password, u.confirmPassword)
+        registerPage.submitRegistration()
+
+        cy.url().should('include', '/signup')
+    })
+
+    it('Tentativa de registro com informações incompletas', () => {
+        registerPage.accessRegisterPage()
+        registerPage.submitRegistration()
+        registerPage.checkIncompleteError()
+    })
+})
